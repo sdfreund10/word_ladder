c951339354fa2b89820dd285f35d7c05eb033771
Stephen Freund <sdfreund10@gmail.com>
2020-01-31 21:24:31 -0500

    Add some mildly gross colors

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,32 +11,39 @@ function App() {
   return html`
     <div className="app">
       <div className="main">
-        <h1>
-          Word Ladder
-        </h1>
-        <${Background} />
-        <h2>Try it!</h4>
-        <${Form} />
-        <h2>API</h2>
-        <p>
-          This site provides an API to provide the shortest possible solution to
-          this puzzle given a start and an end word.
-          If there are multiple paths tied for the shortest chain,
-          this API will return all solutions.
-        </p>
-        <h3>GET /paths/new</h3>
-        <${ParametersTable} />
-        <${ResponsesTable} />
-
-        <h3>A note on caching</h3>
-        <p>
-          Solving a word ladder puzzle can be very computationally expensive.
-          This API does no precalculation and manually computes the solution every time.
-          To make this a little faster and not get clogged up with multiple long running requests,
-          the API uses a simple caching function. This cache is small and will not last long,
-          but should make subsequent requests given the same start and end word
-          much faster.
-        </p>
+        <div className="card">
+          <h1>
+            Word Ladder
+          </h1>
+          <${Background} />
+        </div>
+        <div className="card">
+          <h2>Try it!</h4>
+          <${Form} />
+        </div>
+        <div className="card">
+          <h2>API</h2>
+          <p>
+            This site provides an API to provide the shortest possible solution to
+            this puzzle given a start and an end word.
+            If there are multiple paths tied for the shortest chain,
+            this API will return all solutions.
+          </p>
+          <h3>GET /paths/new</h3>
+          <${ParametersTable} />
+          <${ResponsesTable} />
+        </div>
+        <div className="card">
+          <h3>A note on caching</h3>
+          <p>
+            Solving a word ladder puzzle can be very computationally expensive.
+            This API does no precalculation and manually computes the solution every time.
+            To make this a little faster and not get clogged up with multiple long running requests,
+            the API uses a simple caching function. This cache is small and will not last long,
+            but should make subsequent requests given the same start and end word
+            much faster.
+          </p>
+        </div>
       </div>
     </div>
   `;
